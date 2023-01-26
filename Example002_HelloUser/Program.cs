8146c2026e7b2e64af23e76b1f703c213d5a1016
Ivan <sheva134@mail.ru>
2023-01-27 00:18:05 +0300

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿// Console.WriteLine("Введите ваше имя");
+// string username =  Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+// или
+
+Console.Write("Введите ваше имя");
+string username =  Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
